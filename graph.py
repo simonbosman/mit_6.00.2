1d0f945edcedc09ce7d30dbbefdc5abe48cdb371
Simon Bosman <simon@debian.codedigger.local>
2015-04-13 20:48:12 +0200

    some graphfun on MIT campus

---
+++ graph.py
@@ -0,0 +1,107 @@
+class Node(object):
+    def __init__(self, name):
+        self.name = str(name)
+    def getName(self):
+        return self.name
+    def __str__(self):
+        return self.name
+
+class Edge(object):
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+    def getSource(self):
+        return self.src
+    def getDestination(self):
+        return self.dest
+    def __str__(self):
+        return str(self.src) + '->' + str(self.dest)
+
+class WeightedEdge(Edge):
+    def __init__(self, src, dest, weight = 1.0):
+        self.src = src
+        self.dest = dest
+        self.weight = weight
+    def getWeight(self):
+        return self.weight
+    def __str__(self):
+        return str(self.src) + '->(' + str(self.weight) + ')'\
+            + str(self.dest)
+
+class Digraph(object):
+    def __init__(self):
+        self.nodes = set([])
+        self.edges = {}
+    def addNode(self, node):
+        if node in self.nodes:
+            raise ValueError('Duplicate node')
+        else:
+            self.nodes.add(node)
+            self.edges[node] = []
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not(src in self.nodes and dest in self.nodes):
+            raise ValueError('Node not in graph')
+        self.edges[src].append(dest)
+    def childrenOf(self, node):
+        return self.edges[node]
+    def hasNode(self, node):
+        return node in self.nodes
+    def __str__(self):
+        res = ''
+        for k in self.edges:
+            for d in self.edges[k]:
+                res = res + str(k) + '->' + str(d) + '\n'
+        return res[:-1]
+
+class Graph(Digraph):
+    def addEdge(self, edge):
+        Digraph.addEdge(self, edge)
+        rev = Edge(edge.getDestination(), edge.getSource())
+        Digraph.addEdge(self, rev)
+
+
+def printPath(path):
+    # a path is a list of nodes
+    result = ''
+    for i in range(len(path)):
+        if i == len(path) - 1:
+            result = result + str(path[i])
+        else:
+            result = result + str(path[i]) + '->'
+    return result
+
+
+#Make some nodes
+nodes = []
+nodes.append(Node("ABC")) # nodes[0]
+nodes.append(Node("ACB")) # nodes[1]
+nodes.append(Node("BAC")) # nodes[2]
+nodes.append(Node("BCA")) # nodes[3]
+nodes.append(Node("CAB")) # nodes[4]
+nodes.append(Node("CBA")) # nodes[5]
+
+#Put the nodes in a graph
+g = Graph()
+for n in nodes:
+    g.addNode(n)
+    
+#Make some edges for every node only whose permutations are between elements in the set beside each other
+for idxSelf in range(len(nodes)):
+    #check if there is an allowed edge to another node
+    for idxOther in range(len(nodes)):
+        #Only test other nodes
+        if idxSelf == idxOther:
+            break
+        else:
+            nodeSelf = nodes[idxSelf].getName()
+            nodeOther = nodes[idxOther].getName()
+            if nodeSelf[1] == nodeOther[0] and nodeSelf[2] == nodeOther[2]:
+                #Make the edge
+                g.addEdge(Edge(nodes[idxSelf], nodes[idxOther]))
+            elif nodeSelf[2] == nodeOther[1] and nodeSelf[0] == nodeOther[0]:
+                #Make the edge
+                g.addEdge(Edge(nodes[idxSelf], nodes[idxOther]))
+      
+print g
